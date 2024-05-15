d2876aead42694abace0da17789f826c4f1fdbb4
João Santos <jdcg.santos@gmail.com>
2024-05-15 20:27:11 +0100

    touch events

--- src/components/Sounds.jsx
+++ src/components/Sounds.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+/*import { useEffect, useState } from 'react'
 import '../styles/create.css'
 import { useRef } from 'react'
 function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
@@ -13,7 +13,7 @@ function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
     setSelectedGenre('')
     setSelectedCategory('')
   }*/
-  useEffect(() => {
+  /*useEffect(() => {
     const preloadSounds = () => {
       const soundsToPreload = sounds
         .filter((sound) => sound.genre === selectedGenre && sound.category === selectedCategory)
@@ -56,7 +56,7 @@ function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
     handleSoundClick(soundUrl)
   }*/
 
-  const handleCheckboxChange = (event, sound) => {
+  /*const handleCheckboxChange = (event, sound) => {
     event.stopPropagation(); // Prevent event bubbling to parent elements
     
     if (selectedSounds.length >= 24 && !selectedSounds.some(selectedSound => selectedSound.soundUrl === sound.soundUrl)) {
@@ -72,7 +72,7 @@ function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
     playSound(soundUrl)
   }*/
 
-  const playSound = soundUrl => {
+ /* const playSound = soundUrl => {
     const audio = new Audio(soundUrl)
     audio.currentTime = 0
     audio.play().catch(error => console.error(`Failed to play sound: ${error}`))
@@ -145,7 +145,6 @@ function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
 
 
                     
-                  {/*  <span onClick={event => handleSoundNameClick(event, sound.soundUrl)}></span>*/}
                     <div className='form-check form-switch'>
                       <input
                         className='form-check-input'
@@ -187,7 +186,6 @@ function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
                     >
                      <span> {sound.name}</span>
                     </button>
-                     {/*<span onClick={event => handleSelectedSoundClick(event, sound.soundUrl)}></span>*/}
                   </li>
                 ))}
               
@@ -198,4 +196,175 @@ function Sounds({ sounds,  handleSoundSelect, selectedSounds }) {
     </div>
   )
 }
-export default Sounds
+export default Sounds*/ 
+
+import { useEffect, useState, useRef } from 'react';
+import '../styles/create.css';
+
+function Sounds({ sounds, handleSoundSelect, selectedSounds }) {
+  const [selectedGenre, setSelectedGenre] = useState('Basico');
+  const [selectedCategory, setSelectedCategory] = useState('Basic');
+  const [maxSoundsReached, setMaxSoundsReached] = useState(false);
+  const audioRefs = useRef({});
+
+  useEffect(() => {
+    const preloadSounds = async () => {
+      const soundsToPreload = sounds
+        .filter((sound) => sound.genre === selectedGenre && sound.category === selectedCategory)
+        .map((sound) => sound.soundUrl);
+
+      await Promise.all(
+        soundsToPreload.map(async (soundUrl) => {
+          if (!audioRefs.current[soundUrl]) {
+            const audio = new Audio(soundUrl);
+            await audio.load();
+            audioRefs.current[soundUrl] = audio;
+          }
+        })
+      );
+    };
+
+    preloadSounds();
+  }, [sounds, selectedGenre, selectedCategory]);
+
+  const handleGenreChange = (event) => {
+    setSelectedGenre(event.target.value);
+    setSelectedCategory(''); // Reset selected category when genre changes
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  const handleTouchStart = (soundUrl) => {
+    playSound(soundUrl);
+};
+  const handleCheckboxChange = (event, sound) => {
+    event.stopPropagation(); // Prevent event bubbling to parent elements
+    
+    if (selectedSounds.length >= 27 && !selectedSounds.some(selectedSound => selectedSound.soundUrl === sound.soundUrl)) {
+      setMaxSoundsReached(true);
+      return;
+    }
+    
+    setMaxSoundsReached(false); // Reset the state if the limit is not reached
+    handleSoundSelect(sound);
+  }
+
+  const playSound = (soundUrl) => {
+    const audio = new Audio(soundUrl);
+    audio.currentTime = 0;
+    audio.play().catch(error => console.error(`Failed to play sound: ${error}`));
+  };
+
+  const handlePlayButtonClick = (soundUrl) => {
+    playSound(soundUrl);
+  };
+
+  const groupedSounds = {};
+  sounds.forEach(sound => {
+    if (!groupedSounds[sound.genre]) {
+      groupedSounds[sound.genre] = {};
+    }
+    if (!groupedSounds[sound.genre][sound.category]) {
+      groupedSounds[sound.genre][sound.category] = [];
+    }
+    groupedSounds[sound.genre][sound.category].push(sound);
+  });
+
+  return (
+    <div>
+      <div className='selectCards'>
+        <div className='categoriesAndGenres'>
+          <h4>Select Genre and category</h4>
+          <div className='category'>
+            Categories
+            <select value={selectedGenre} onChange={handleGenreChange}>
+              <option value=''>Select Genre</option>
+              {Object.keys(groupedSounds).map(genre => (
+                <option key={genre} value={genre}>
+                  {genre}
+                </option>
+              ))}
+            </select>
+          </div>
+          {selectedGenre && (
+            <div className='genre'>
+              Genres
+              <select value={selectedCategory} onChange={handleCategoryChange}>
+                <option value=''>Select Category</option>
+                {Object.keys(groupedSounds[selectedGenre]).map(category => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+        </div>
+
+        <div className='availableSounds'>
+          {selectedGenre && selectedCategory && (
+            <>
+              <h3>Select Sounds:</h3>
+              <ul>
+                {groupedSounds[selectedGenre][selectedCategory].map(sound => (
+                  <li key={sound.soundUrl} className='selected-sound-item'>
+                    <button
+                      className='selected-sound-item'
+                      type="button" 
+                      onTouchStart={() => handleTouchStart(sound.soundUrl)}
+
+                    >
+                      <span>{sound.name}</span>
+                    </button>
+                    <div className='form-check form-switch'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        checked={selectedSounds.some(
+                          selectedSound => selectedSound.soundUrl === sound.soundUrl
+                        )}
+                        onChange={event => handleCheckboxChange(event, sound)}
+                        id={`switch-${sound.soundUrl}`}
+                      />
+                      <label
+                        className='form-check-label'
+                        htmlFor={`switch-${sound.soundUrl}`}
+                      ></label>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+        {maxSoundsReached && (
+          <p style={{ color: 'red' }}>You can only select a maximum of 27 sounds.</p>
+        )}
+        {selectedSounds.length > 0 && (
+          <div className='selectedSounds'>
+            <h3>Selected Sounds:</h3>
+            <ul>
+              {selectedSounds.map(sound => (
+                <li key={sound.soundUrl}>
+                  <button
+                    className='selected-sound-item'
+                    type="button" 
+                    onClick={() => handlePlayButtonClick(sound.soundUrl)}
+                    onTouchStart={() => handleTouchStart(sound.soundUrl)}
+
+                  >
+                    <span>{sound.name}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Sounds;
+
